e5149b94a8bf789fc1c2c70109aeb6e5bd07a5ed
LounesBrahimi <Lounesnewgs5@gmail.com>
2021-10-23 12:36:22 +0200

    implementation of the interface dao for the product

---
+++ microcommerce/src/main/java/com/ecomerceexample/microcommerce/dao/ProductDaoImpl.java
@@ -0,0 +1,33 @@
+package com.ecomerceexample.microcommerce.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import com.ecomerceexample.microcommerce.model.Product;
+
+@Repository
+public class ProductDaoImpl implements ProductDao{
+
+	public static List<Product> products = new ArrayList<Product>();
+	
+	static {
+		products.add(new Product(1, "dell Camputer", 1000));
+		products.add(new Product(2, "apple Camputer", 1500));
+		products.add(new Product(3, "iphone 13", 1500));
+	}
+	
+	@Override
+	public List<Product> findAll() {
+		return products;
+	}
+
+	@Override
+	public Product findProductById(int id) {
+		return null;
+	}
+
+	@Override
+	public Product save(Product product) {
+		return null;
+	}
+}
--- microcommerce/src/main/java/com/ecomerceexample/microcommerce/web/controller/ProductController.java
+++ microcommerce/src/main/java/com/ecomerceexample/microcommerce/web/controller/ProductController.java
@@ -1,14 +1,29 @@
 package com.ecomerceexample.microcommerce.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecomerceexample.microcommerce.dao.ProductDao;
 import com.ecomerceexample.microcommerce.model.Product;
 
 @RestController
 public class ProductController {
 
+	@Autowired
+	private ProductDao productDao;
+	
+	/*
+	 * all the products
+	 * */
+	@GetMapping(value = "Products")
+	public List<Product> listPorducts() {
+		return productDao.findAll();
+	}
+	
 	/*
 	 * Method that answers to product/{id}
 	 * */
